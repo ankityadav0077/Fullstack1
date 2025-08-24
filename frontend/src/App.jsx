7d56cd76ae3c01bb7192a33fe277050f7a87cf52
Ankit Yadav <ankityadavok5@gmail.com>
2025-08-24 17:22:43 +0530

    add store to track the improvement of task

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Tasktodo from './pages/Tasktodo'
+import Features from './pages/Featuers'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <Tasktodo/>   
+    <Features/>
     </>
   )
 }
